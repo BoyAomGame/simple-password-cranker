e311ef51cf069d97630988008b11f9a41c286b9d
CallmeQ <sompongbbx@gmail.com>
2023-01-06 13:05:56 +0700

    Make main.py code look better

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-
-#Web
+import time
 import random
-import numpy as np 
 
-password = "Target123"
+#This function is used for using random word from wordlist.
 def cranker():
     wordlist = ["12345", "BobTring", "Elonload", "I LIKE BOB", "Password", "Target123", "Traget123", "Sus"]
     random_word = random.choice(wordlist)
     return random_word
 cranker()
-print(cranker())
 
+#This function checking password from function Cranker
 def checker():
     password = "Target123"
     while cranker() != password:
         print("Password incorrect! Retrying")
         print("Trying:", cranker())
-    
+        time.sleep(5)
     print("Password Correct!")
-    print("You can enter correct password", password)
+    print("Password that correct is ", password)
 checker()
